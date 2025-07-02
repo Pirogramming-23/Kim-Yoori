0ce94b19a6c2e9f229790e20081880bd16b4ed23
Github yoyoori <smwenddl0405@naver.com>
2025-07-02 23:53:24 +0900

    [add] 숫자 판정 및 결과 출력

--- 숫자야구 게임/script.js
+++ 숫자야구 게임/script.js
@@ -37,7 +37,7 @@ function resetGame(){
 
 // 2. 입력
 // 사용자는 3개의 숫자를 각 input 칸에 입력
-// #submit_button 클릭 시 check_numbers() 실행
+// #submit_button 클릭 시 check_numbers() 실행 --> 이거 이벤트 리스너로 연결해줘야함 !!!!
 
 // 3. 유효성 검사
 // 입력칸 중 하나라도 비어 있으면 input만 비우고 넘어감
@@ -52,17 +52,40 @@ function check_numbers(){
         return;
     }
 
+    // 4. 숫자 판정
+    // 컴퓨터 정답과 비교하기
+    // 숫자와 위치 일치하면 스트라이크(S)
+    // 숫자만 일지하면 볼(b)
+    // 아무것도 일치하지 않으면 "O" 아웃
+
+    let input = [parseInt(n1), parseInt(n2), parseInt(n3)];
+    let strikes = 0;
+    let balls = 0;
+
+    for(let i=0; i < 3; i++){
+        if (input[i] === answer[i]) {
+            strikes ++;
+        }
+        else if (answer.includes(input[i])) {
+            balls ++;
+        }
+        
+    }
+    
+    // 5. 결과 출력
+    // 결과 메시지 출력 (예: 1S 2B /또는 O)
+    // 시도 횟수 -1
+    if (strikes === && balls === 0){
+       resultText = 'O';
+    }
+    else {
+        resultText = `${strikes}S ${balls}B`;
+    }
+    
+    document.getElementById('result').innerHTML += resultText + "<br>";
+    attempts --;
+    document.getElementById('attempts').innerText = attempts;
 }
-
-// 4. 숫자 판정
-// 컴퓨터 정답과 비교하기
-// 숫자와 위치 일치하면 스트라이크(S)
-// 숫자만 일지하면 볼(b)
-// 아무것도 일치하지 않으면 "O" 아웃
-
-// 5. 결과 출력
-// 결과 메시지 출력 (예: 1S 2B /또는 O)
-// 시도 횟수 -1
 
 // 6. 게임 종료 판단
 // 3 스트라이크 시 승리 이미지 
