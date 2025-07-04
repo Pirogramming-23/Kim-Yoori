ee68e0d5d0164a2cd7bcd48b4ce17d381ca2f2f1
Github yoyoori <smwenddl0405@naver.com>
2025-07-04 16:12:13 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,45 +1,36 @@
 #1단계
 num = 0
 
-#3단계
+#6단계
+#배스킨라빈스31 게임은 참여자가 번갈아가며 숫자를 부른다. 
+#게임이 끝날 때까지 playerA와 playerB에게 번갈아가며 부를 숫자의 개수를 입력 받는 코드를 작성하여라.
+# 게임은 누군가 31을 부르면 끝난다.
+game_count = 1
+play_turn = "playerA"
+
 while True:
-    try:
-        #2단계
-        num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
-        if num not in [1,2,3]:
-            print("1,2,3 중 하나를 입력하세요")
-        else:
-            break
-    except ValueError:
-        print("정수를 입력하세요")
+    #3단계
+    while True:
+        try:
+            #2단계
+            num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
+            if num not in [1,2,3]:
+                print("1,2,3 중 하나를 입력하세요")
+            else:
+                break
+        except ValueError:
+            print("정수를 입력하세요")
 
-#4단계
-count = 0
-for i in range (num):
-    print("playerA : %d" % (i+1))
-count = i + 1 #player A가 부른 마지막 숫자
+    #4단계
+    for i in range (num):
+        print("%s : %d" % (play_turn, game_count))
+        game_count += 1
 
-#5단계
-# 아래의 내용이 만족하도록 코드를 작성하여라.
-# 1에서 3사이의 정수를 입력 받는 코드를 작성하여라.
-# 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :
-# 정수를 입력하지 않는 경우, 정수를 입력하세요를 출력한다.
-# 1,2,3을 입력하지 않는 경우, 1,2,3 중 하나를 입력하세요를 출력한다.
-# 올바른 값이 입력될 때까지 입력을 요구한다.
-# 변수 num을 이용하여 입력한 수만큼 숫자를 출력하는 코드를 작성하여라.
-# 예를 들어, 2단계에서 playerA가 3을 입력한 상태에서, playerB가 2를 입력하면 4,5를 아래와 같은 형식으로 출력하여라.
-# playerB : 4
-# playerB : 5
-while True:
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
-        if num not in [1,2,3]:
-            print("1,2,3 중 하나를 입력하세요")
-        else:
-            break
-    except ValueError:
-        print("정수를 입력하세요")
+    if game_count > 31:
+       break
 
-for i in range(num):
-    print("playerB : %d" %(count+i+1))
-
+    #차례 번갈아 가기
+    if play_turn == "playerA":
+        play_turn = "playerB"
+    else:
+        play_turn = "playerA"
