677e98b38c1ff90cff8a87e7cb67015542b7984a
Github yoyoori <smwenddl0405@naver.com>
2025-07-04 17:23:14 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,44 +1,44 @@
-#8단계 
-#6단계까지 중복되는 코드를 찾아 함수로 만들어라. 이때, 함수 이름은 brGame으로 한다.
-def brGame(turn, count):
-    #1단계
-    num = 0
-    while True:
-        try:
-            #2단계
-            num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
-            if num not in [1,2,3]:
-                print("1,2,3 중 하나를 입력하세요")
-            else:
-                break
-        except ValueError:
-            print("정수를 입력하세요")
+#9단계
+import random
 
-    #4단계
+def brGame(turn, count):
+    num = 0
+    if turn == 'player':
+        while True:
+            try:
+                num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
+                if num not in [1,2,3]:
+                    print("1,2,3 중 하나를 입력하세요")
+                else:
+                    break
+            except ValueError:
+                print("정수를 입력하세요")
+    elif turn == 'computer':
+        num = random.randint(1,3)
+
     for i in range (num):
+        if count == 31:
+            print("%s : %d" % (turn, count))
+            return count + 1
         print("%s : %d" % (turn, count))
         count += 1
 
     return count
 
-#6단계
 game_count = 1
-play_turn = "playerA"
+play_turn = "player"
 
 while True:
-    #3단계
     game_count = brGame(play_turn, game_count)
 
-    #7단계
     if game_count > 31:
-        if play_turn == "playerA":
-           print("playerB win!")
+        if play_turn == "player":
+           print("computer win!")
         else:
-           print("playerA win!")
+           print("player win!")
         break
 
-    #차례 번갈아 가기
-    if play_turn == "playerA":
-        play_turn = "playerB"
+    if play_turn == "player":
+        play_turn = "computer"
     else:
-        play_turn = "playerA"
+        play_turn = "player"
