bc1af74978094cb9871442da5732cb5484a2b6bb
Github yoyoori <smwenddl0405@naver.com>
2025-07-11 16:04:13 +0900

    리뷰 모델 만들기 완료

--- myMovieReviews/reviews/models.py
+++ myMovieReviews/reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Reveiw(models.Model):
+    title = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    director = models.CharField(max_length=100)
+    actors = models.CharField(max_length=200)
+    genre = models.CharField(max_length=50)
+    rating = models.IntegerField()
+    running_time = models.IntegerField(help_text="단위: 분")
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
