289a6f4ccab10f1aee93e308ae9d424b99301509
Github yoyoori <smwenddl0405@naver.com>
2025-07-11 19:58:51 +0900

    기능1 리뷰 리스트 출력 구현

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ReviewListView.as_view(), name="review_list"),
-    path('<int:pk>/', views.ReviewDetailView.as_view(), name='review_detail'),
-    path('new/', views.ReviewCreateView.as_view(), name='review_create'),
-    path('<int:pk>/edit/', views.ReviewUpdateView.as_view(), name='review_update'),
-    path('<int:pk>/delete/', views.ReviewDeleteView.as_view(), name='review_delete'),
+    path('', views.review_list, name='review_list'),
+    #path('new/', views.review_create, name='review_create'),
 ]
