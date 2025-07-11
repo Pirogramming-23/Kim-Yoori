8c3870d84f3b88eeaf1f62b39f737cb8505d8a85
Github yoyoori <smwenddl0405@naver.com>
2025-07-11 20:16:35 +0900

    기능3 구현

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.review_list, name='review_list'),
-    #path('new/', views.review_create, name='review_create'),
+    path('new/', views.review_create, name='review_create'),
 ]
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
+# review_list 페이지
 def review_list(request):
     reviews = Review.objects.all()
-    return render(request, 'reviews/review_list.html', {'reviews' : reviews})
+    return render(request, 'reviews/review_list.html', {'reviews':reviews})
+
+# review_create 페이지
+def review_create(request):
+    if request.method == 'POST' :
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('review_list')
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/review_form.html', {'form' : form})
