cee61f8ad1cc6c1e4423406d73a79b3a81382f88
Github yoyoori <smwenddl0405@naver.com>
2025-07-16 23:10:09 +0900

    [add] 기능 2

--- SWIDEA_SITE/ideas/views.py
+++ SWIDEA_SITE/ideas/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Idea
 
-# Create your views here.
+# 아이디어 리스트 출력
 def idea_list(request):
-    ideas = Idea.objects.all().order_by('-created_at') #정렬 기준은 최신순으로 
+    #정렬 기준 받기
+    sort = request.GET.get('sort', 'latest')
+
+    if sort == 'name':
+        ideas = Idea.objects.all().order_by('title')
+    elif sort == 'interest':
+        ideas = Idea.objects.all().order_by('-interest')
+    elif sort == 'oldest':
+        ideas = Idea.objects.all().order_by('created_at')
+    else:
+        ideas = Idea.objects.all().order_by('-created_at')
+
     return render(request, 'ideas/idea_list.html', {'ideas':ideas})
