25d9d78714122b529c920616f8cbdfa2f7ab6287
Github yoyoori <smwenddl0405@naver.com>
2025-07-21 12:12:05 +0900

    [chore] 프로젝트 기본 세팅 완료

---
+++ piro23_Ajax_Pirostagram/posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return f"{self.author.username}'s post"
+
---
+++ piro23_Ajax_Pirostagram/posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts/index.html', {'posts':posts})
+
